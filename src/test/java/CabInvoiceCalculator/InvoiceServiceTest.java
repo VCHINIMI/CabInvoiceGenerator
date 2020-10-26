5f3c9ed176742ab5886fb27c0918ae2df76bab71
VCHINIMI <vinaychinimilli@gmail.com>
2020-10-26 19:35:10 +0530

    [Vinay]Added: Test Case for Calculation of total fare

--- src/test/java/CabInvoiceCalculator/InvoiceServiceTest.java
+++ src/test/java/CabInvoiceCalculator/InvoiceServiceTest.java
@@ -7,5 +7,17 @@ import org.junit.Test;
 public class InvoiceServiceTest {
 
 	InvoiceService invoiceService = null;
+
+	@Before
+	public void initialize() {
+		invoiceService = new InvoiceService();
+	}
 	
+	@Test
+	public void givenDistanceandTimeShouldReturnFare() {
+		double distance = 2.0;
+		int time = 5;
+		double fare = invoiceService.calculateFare(distance, time);
+		assertEquals(25, fare,0.0);
+	}
 }
