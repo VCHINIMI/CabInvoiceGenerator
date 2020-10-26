ee78f01fbf93402922458542cf7482a4785271bf
VCHINIMI <vinaychinimilli@gmail.com>
2020-10-26 23:31:18 +0530

    [Vinay]Added: Retreiving Invoice from USERId in the Invoice Service class

--- src/main/java/CabInvoiceCalculator/InvoiceService.java
+++ src/main/java/CabInvoiceCalculator/InvoiceService.java
@@ -4,7 +4,7 @@ public class InvoiceService {
 	private static final int COST_PER_TIME = 1;
 	private static final double MINIMUM_FARE = 5;
 	private static final double MINIMUM_COST_PER_KILOMETER = 10;
-	private RideRepository rideRepository;
+	private RideRepository rideRepository = new RideRepository();
 
 	public double calculateFare(double distance, int time) {
 		double totalFare = distance * MINIMUM_COST_PER_KILOMETER + time * COST_PER_TIME;
@@ -21,5 +21,14 @@ public class InvoiceService {
 
 	public InvoiceSummary calculateFareInvoiceSummary(Ride[] rides) {
 		return new InvoiceSummary(calculateFare(rides), rides.length);
+	}
+
+	public void addRides(String userId, Ride[] rides) {
+		rideRepository.addRides(userId, rides);
+	}
+
+	public InvoiceSummary getInvoiceFromUserId(String userId) {
+		return (this.rideRepository.getRidesData(userId) == null) ? null
+				: this.calculateFareInvoiceSummary(this.rideRepository.getRidesData(userId));
 	}
 }
