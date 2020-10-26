85ed15d363ec6f8b815670b5c9448293d7629546
VCHINIMI <vinaychinimilli@gmail.com>
2020-10-27 00:36:57 +0530

    [Vinay]Added: CalculateFare method for premium rides

--- src/main/java/CabInvoiceCalculator/InvoiceService.java
+++ src/main/java/CabInvoiceCalculator/InvoiceService.java
@@ -1,9 +1,15 @@
 package CabInvoiceCalculator;
 
+import CabInvoiceCalculator.Ride.TYPE;
+
 public class InvoiceService {
 	private static final int COST_PER_TIME = 1;
+	private static final int COST_PER_TIME_PREMIUM = 2;
 	private static final double MINIMUM_FARE = 5;
+	private static final double MINIMUM_FARE_PREMIUM = 20;
 	private static final double MINIMUM_COST_PER_KILOMETER = 10;
+	private static final double MINIMUM_COST_PER_KILOMETER_PREMIUM = 15;
+
 	private RideRepository rideRepository = new RideRepository();
 
 	public double calculateFare(double distance, int time) {
@@ -11,10 +17,16 @@ public class InvoiceService {
 		return Math.max(MINIMUM_FARE, totalFare);
 	}
 
+	public double calculateFareForPremium(double distance, int time) {
+		double totalFare = distance * MINIMUM_COST_PER_KILOMETER_PREMIUM + time * COST_PER_TIME_PREMIUM;
+		return Math.max(MINIMUM_FARE_PREMIUM, totalFare);
+	}
+
 	public double calculateFare(Ride[] rides) {
 		double totalFare = 0;
 		for (Ride ride : rides) {
-			totalFare += calculateFare(ride.distance, ride.time);
+			totalFare += (ride.type == TYPE.REGULAR) ? calculateFare(ride.distance, ride.time)
+					: calculateFareForPremium(ride.distance, ride.time);
 		}
 		return totalFare;
 	}
