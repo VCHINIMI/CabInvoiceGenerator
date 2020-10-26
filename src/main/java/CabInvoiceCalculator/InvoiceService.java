59a8e3a6b96c4fd50e7dca540e8f7a1b116bf84e
VCHINIMI <vinaychinimilli@gmail.com>
2020-10-26 19:38:07 +0530

    [Vinay]Added: CalculateFare method for InvoiceService class corresponding to the test case

--- src/main/java/CabInvoiceCalculator/InvoiceService.java
+++ src/main/java/CabInvoiceCalculator/InvoiceService.java
@@ -1,5 +1,13 @@
 package CabInvoiceCalculator;
 
 public class InvoiceService {
+	private static final int COST_PER_TIME = 1;
+	private static final double MINIMUM_FARE = 5;
+	private static final double MINIMUM_COST_PER_KILOMETER = 10;
+	private RideRepository rideRepository;
 
+	public double calculateFare(double distance, int time) {
+		double totalFare = distance * MINIMUM_COST_PER_KILOMETER + time * COST_PER_TIME;
+		return Math.max(MINIMUM_FARE, totalFare);
+	}
 }
