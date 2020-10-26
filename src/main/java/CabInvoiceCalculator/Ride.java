99f8ddaaabb1e32d13cf87d5da9a26e3d6e886cf
VCHINIMI <vinaychinimilli@gmail.com>
2020-10-26 19:46:32 +0530

    [Vinay]Added: Ride class for initialising Rides os a user

--- src/main/java/CabInvoiceCalculator/Ride.java
+++ src/main/java/CabInvoiceCalculator/Ride.java
@@ -1,5 +1,12 @@
 package CabInvoiceCalculator;
 
 public class Ride {
-	
+	public double distance;
+	public int time;
+
+	public Ride(double distance, int time) {
+		super();
+		this.distance = distance;
+		this.time = time;
+	}
 }
