affc03a6f4984dbf6ff2efe72b34c95c5d6fd09d
VCHINIMI <vinaychinimilli@gmail.com>
2020-10-27 00:35:54 +0530

    [Vinay]Added: ENUM for Ride type PREMIUM/NORMAL

--- src/main/java/CabInvoiceCalculator/Ride.java
+++ src/main/java/CabInvoiceCalculator/Ride.java
@@ -1,12 +1,19 @@
 package CabInvoiceCalculator;
 
 public class Ride {
+	public enum TYPE{
+		REGULAR,
+		PREMIUM
+	}
+	
+	public TYPE type;
 	public double distance;
 	public int time;
 
-	public Ride(double distance, int time) {
+	public Ride(double distance, int time, TYPE type) {
 		super();
 		this.distance = distance;
 		this.time = time;
+		this.type = type;
 	}
 }
